801514d4a722279dcd21e1850b6da8098bd3e623
DELL <gor_nick987@mail.ru>
2017-11-18 23:56:31 +0300

    proc: getSimilarBooksByTags

--- DB/scripts/storedProcedures_PROD.sql
+++ DB/scripts/storedProcedures_PROD.sql
@@ -9,17 +9,18 @@ GO
 CREATE PROC [dbo].[selectBooksByGenre] 
     @genre NVARCHAR(50)
 AS 
-	SET NOCOUNT ON  --отлючить вывод кол-ва обработанных строк
-	SET XACT_ABORT ON  --ролбэк транзакции и прекращение процедуры
+	begin
+		SET NOCOUNT ON  --отлючить вывод кол-ва обработанных строк
+		SET XACT_ABORT ON  --ролбэк транзакции и прекращение процедуры
 
-	SELECT b.[Name], b.[Year]
-		FROM [dbo].[Books] as b
-			INNER JOIN [dbo].[Book_Genres] as bg
-		ON b.id = bg.Book_Id
-			INNER JOIN [dbo].[Genres] as g
-		ON g.[Id] = bg.[Genre_Id]
-		WHERE g.[Genre] = @genre	
-
+		SELECT b.[Name], b.[Year]
+			FROM [dbo].[Books] as b
+				INNER JOIN [dbo].[Book_Genres] as bg
+			ON b.id = bg.Book_Id
+				INNER JOIN [dbo].[Genres] as g
+			ON g.[Id] = bg.[Genre_Id]
+			WHERE g.[Genre] = @genre	
+	end;
 GO
 
 
@@ -47,13 +48,13 @@ AS
 	end;
 GO
 
-----------------------[Получить книгу с 3/3 тэгами: точное соответствие по тематике] ------------------------------------
-IF OBJECT_ID('[dbo].[selectBooksByTag]') IS NOT NULL
+----------------------[Поиск книг по заданным тегам с учетом частоты их использования] ------------------------------------
+IF OBJECT_ID('[dbo].[selectSimilarBooksByTags]') IS NOT NULL
 BEGIN 
-    DROP PROC [dbo].[selectBooksByTag] 
+    DROP PROC [dbo].[selectSimilarBooksByTags] 
 END 
 GO
-CREATE PROC [dbo].[selectBooksByTag] 
+CREATE PROC [dbo].[selectSimilarBooksByTags] 
     @tag1 NVARCHAR(50),
     @tag2 NVARCHAR(50),
     @tag3 NVARCHAR(50)
@@ -62,17 +63,16 @@ AS
 		SET NOCOUNT ON  --отлючить вывод кол-ва обработанных строк
 		SET XACT_ABORT ON  --ролбэк транзакции и прекращение процедуры
 
-		SELECT  count(b.name), t.name
-			FROM [Books] as b
-				INNER JOIN [Book_Tags] as bt
-			ON b.[Id] = bt.[Book_Id]
-				INNER JOIN [Tags] as t
-			ON t.[Id] = bt.[Tag_Id] group by t.name  --получить все книги по тегу
-			WHERE t.[Name] = @tag 
-
+		  SELECT b.name, count(t.id) as Concurrences FROM [Books] as b, [Book_Tags] as bt, [Tags] as t
+		    WHERE bt.tag_id = t.id
+		    AND (t.name IN (@tag1, @tag2, @tag3))
+			AND b.id = bt.book_id 
+			GROUP BY b.name
+			order by Concurrences desc
 	end;
 GO
 
 
 exec [dbo].[selectBooksByTag] 'leo';
 exec [dbo].[selectBooksByGenre] 'Thriller';
+exec [dbo].[selectSimilarBooksByTags]  'integer', 'leo', 'id'
